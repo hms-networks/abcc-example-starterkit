e62db41e89d6f094becd32b0da479031349c8501
BerndWernerGuenther <beg@hms-networks.de>
2026-02-19 14:35:54 +0100

    corrections to the code extension for structured ADIs

--- src/abcc_adaptation/abcc_driver_config.h
+++ src/abcc_adaptation/abcc_driver_config.h
@@ -17,18 +17,28 @@
 #include "abcc_types.h"
 #include "abp.h"
 
-//added to support structured ADIs; start
+
+/*------------------------------------------------------------------------------
+** Selection of ADI setup: 
+** when defined structured ADIs will be used; 
+** if not defined the ADIs for the speed example will be used
+**------------------------------------------------------------------------------
+*/
 #ifndef ABCC_CFG_DRV_STRUCTURED_ADIS
  #define ABCC_CFG_DRV_STRUCTURED_ADIS 1						
 #endif
 
+
+
+/*------------------------------------------------------------------------------
+** Switch dependent enabling of STRUCT_DATA_TYPE
+**------------------------------------------------------------------------------
+*/
 #ifdef ABCC_CFG_DRV_STRUCTURED_ADIS
 #ifndef ABCC_CFG_STRUCT_DATA_TYPE_ENABLED
  #define ABCC_CFG_STRUCT_DATA_TYPE_ENABLED 1     
 #endif
 #endif
-//added to support structured ADIs; end
-
 
 
 /*------------------------------------------------------------------------------
--- src/example_application/abcc_network_data_parameters.c
+++ src/example_application/abcc_network_data_parameters.c
@@ -17,17 +17,93 @@
 #include "abcc_api.h"
 
 
-//added to support structured ADIs; start
-/******************************************************************************************************/
-#ifdef ABCC_CFG_DRV_STRUCTURED_ADIS
-
-
-/*
-** Example with all access flags set.
+#ifndef ABCC_CFG_DRV_STRUCTURED_ADIS
+/*******************************************************************************
+** ADI example with Spped and RefSpeed 
+********************************************************************************
 */
-#define ADI_DESCR_ALL_ACCESS  ( ABP_APPD_DESCR_GET_ACCESS | ABP_APPD_DESCR_SET_ACCESS | ABP_APPD_DESCR_MAPPABLE_WRITE_PD | ABP_APPD_DESCR_MAPPABLE_READ_PD )
+
+/******************************************************************************************************/
+#if (  ABCC_CFG_STRUCT_DATA_TYPE_ENABLED || ABCC_CFG_ADI_GET_SET_CALLBACK_ENABLED )
+#error ABCC_CFG_ADI_GET_SET_CALLBACK_ENABLED must be set to 0 and ABCC_CFG_STRUCT_DATA_TYPE_ENABLED set to 0 in order to run this example
+#endif
+
+/*------------------------------------------------------------------------------
+** Data holder for the network data parameters (ADI)
+**------------------------------------------------------------------------------
+*/
+uint16_t appl_iSpeed;
+uint16_t appl_iRefSpeed;
+
+/*------------------------------------------------------------------------------
+** Min, max and default value for appl_aiUint16
+**------------------------------------------------------------------------------
+*/
+static AD_UINT16Type appl_sUint16Prop = { { 0, 0xFFFF, 0 } };
+
+/*-------------------------------------------------------------------------------------------------------------
+** 1. iInstance | 2. pabName | 3. bDataType | 4. bNumOfElements | 5. bDesc | 6. pxValuePtr | 7. pxValuePropPtr
+**--------------------------------------------------------------------------------------------------------------
+*/
+const AD_AdiEntryType ABCC_API_asAdiEntryList[] =
+{
+   {  0x1,  "SPEED",     ABP_UINT16,   1, AD_ADI_DESC___W_G, { { &appl_iSpeed,    &appl_sUint16Prop } } },
+   {  0x2,  "REF_SPEED", ABP_UINT16,   1, AD_ADI_DESC__R_S_, { { &appl_iRefSpeed, &appl_sUint16Prop } } }
+};
 
 
+/*------------------------------------------------------------------------------
+** Example - electric motor control loop
+**------------------------------------------------------------------------------
+*/
+void ABCC_API_CbfCyclicalProcessing()
+{
+    if (ABCC_API_AnbState() == ABP_ANB_STATE_PROCESS_ACTIVE)
+    {
+        /*
+        ** An example of ADI data handling.
+        */
+        if (appl_iSpeed > appl_iRefSpeed)
+        {
+            /*
+            ** Do something that lowers speed.
+            */
+            appl_iSpeed -= 1;
+        }
+        else if (appl_iSpeed < appl_iRefSpeed)
+        {
+            /*
+            ** Do something that increases speed.
+            */
+            appl_iSpeed += 1;
+        }
+    }
+    else
+    {
+        /*
+        ** We are not in process active, the default should be that the motor
+        ** should not run.
+        */
+        appl_iSpeed = 0;
+    }
+}
+/*------------------------------------------------------------------------------
+** Map all adi:s in both directions
+**------------------------------------------------------------------------------
+** 1. AD instance | 2. Direction | 3. Num elements | 4. Start index |
+**------------------------------------------------------------------------------
+*/
+const AD_MapType ABCC_API_asAdObjDefaultMap[] =
+{
+   { 1, PD_WRITE, AD_MAP_ALL_ELEM, 0 },
+   { 2, PD_READ,  AD_MAP_ALL_ELEM, 0 },
+   { AD_MAP_END_ENTRY }
+};
+/******************************************************************************************************/
+
+
+
+#else       
 /*******************************************************************************
 ** Example of ADI mapping structured data type
 ** (ABCC_CFG_STRUCT_DATA_TYPE_ENABLED defined to 1 in abcc_driver_config.h )
@@ -74,8 +150,8 @@ static const AD_StructDataType ABCC_API_AdiStruct1[] =
     ** | 1. pacElementName | 2. bDataType | 3. iNumSubElem | 4. bDesc | 5. bBitOffset | 6. pxValuePtr | 7. pxValuePropPtr |
     ** --------------------------------------------------------------------------------------------------------------------
     */
-    /* Index: 0 */ {  "Element01",  ABP_UINT16,  1,  ADI_DESCR_ALL_ACCESS,  0,  { {  &struct1.iData,  NULL  } } },
-    /* Index: 1 */ {  "Element02",  ABP_SINT32,  1,  ADI_DESCR_ALL_ACCESS,  0,  { {  &struct1.lData,  NULL  } } }
+    /* Index: 0 */ {  "Element01",  ABP_UINT16,  1,  AD_ADI_DESC__RWSG,  0,  { {  &struct1.iData,  NULL  } } },
+    /* Index: 1 */ {  "Element02",  ABP_SINT32,  1,  AD_ADI_DESC__RWSG,  0,  { {  &struct1.lData,  NULL  } } }
 };
 
 
@@ -109,10 +185,10 @@ static const AD_StructDataType ABCC_API_AdiStruct2[] =
     ** | 1. pacElementName | 2. bDataType | 3. iNumSubElem | 4. bDesc | 5. bBitOffset | 6. pxValuePtr | 7. pxValuePropPtr |
     ** --------------------------------------------------------------------------------------------------------------------
     */
-    /* Index: 0 */ {  "Element01",  ABP_BIT5,    1,  ADI_DESCR_ALL_ACCESS,  0,  { {  &struct2.bBitData[0],  NULL  } } }, /* First 5 bits in struct2.bBitData[ 0 ]                                       */
-    /* Index: 1 */ {  "Element02",  ABP_BIT4,    1,  ADI_DESCR_ALL_ACCESS,  5,  { {  &struct2.bBitData[0],  NULL  } } }, /* 4 bits starting at position 5 in struct2.bBitData[ 0 ]                      */
-    /* Index: 2 */ {  "Element03",  ABP_PAD7,    1,  ADI_DESCR_ALL_ACCESS,  1,  { {  NULL,                  NULL  } } }, /* 7 bits padding is needed starting at position 1 in  struct2.bBitData[ 1 ]   */
-    /* Index: 3 */ {  "Element04",  ABP_UINT16,  1,  AD_ADI_DESC____SG,  0,  { {  &struct2.iData,        NULL  } } }  /* 16 bit type must at least be on a 8 bit aligned position                    */
+    /* Index: 0 */ {  "Element01",  ABP_BIT5,    1,  AD_ADI_DESC__RWSG,  0,  { {  &struct2.bBitData[0],  NULL  } } }, /* First 5 bits in struct2.bBitData[ 0 ]                                       */
+    /* Index: 1 */ {  "Element02",  ABP_BIT4,    1,  AD_ADI_DESC__RWSG,  5,  { {  &struct2.bBitData[0],  NULL  } } }, /* 4 bits starting at position 5 in struct2.bBitData[ 0 ]                      */
+    /* Index: 2 */ {  "Element03",  ABP_PAD7,    1,  AD_ADI_DESC__RWSG,  1,  { {  NULL,                  NULL  } } }, /* 7 bits padding is needed starting at position 1 in  struct2.bBitData[ 1 ]   */
+    /* Index: 3 */ {  "Element04",  ABP_UINT16,  1,  AD_ADI_DESC__RWSG,  0,  { {  &struct2.iData,        NULL  } } }  /* 16 bit type must at least be on a 8 bit aligned position                    */
 };
 //added to support structured ADIs; end
 
@@ -124,18 +200,16 @@ static const AD_StructDataType ABCC_API_AdiStruct2[] =
 **------------------------------------------------------------------------------
 */
 const AD_AdiEntryType ABCC_API_asAdiEntryList[] =
-//const AD_AdiEntryType AD_asADIEntryList[] =
 {
     /*
     ** ----------------------------------------------------------------------------------------------------------------------------
     ** | 1. iInstance | 2. pabName | 3. bDataType | 4. bNumOfElements | 5. bDesc | 6.pxValuePtr | 7. pxValuePropPtr | 8. psStruct |
     ** ----------------------------------------------------------------------------------------------------------------------------
     */
-    {  10,  "Struct1",   ABP_BOOL,   2,  ADI_DESCR_ALL_ACCESS,  { { NULL,    NULL } },  ABCC_API_AdiStruct1  },
-    {  20,  "Struct2",   ABP_BOOL,   4,  ADI_DESCR_ALL_ACCESS,  { { NULL,    NULL } },  ABCC_API_AdiStruct2  },
-    {  30,  "NoStruct",  ABP_UINT8,  1,  AD_ADI_DESC____SG,  { { &bData,  NULL } },  NULL  }
+    {  10,  "Struct1",   ABP_BOOL,   2,  AD_ADI_DESC__RWSG,  { { NULL,    NULL } },  ABCC_API_AdiStruct1  },
+    {  20,  "Struct2",   ABP_BOOL,   4,  AD_ADI_DESC__RWSG,  { { NULL,    NULL } },  ABCC_API_AdiStruct2  },
+    {  30,  "NoStruct",  ABP_UINT8,  1,  AD_ADI_DESC__RWSG,  { { &bData,  NULL } },  NULL  }
 };
-//BEGTEST: orig: ADI_DESCR_ALL_ACCESS, AD_ADI_DESC____SG
 
 
 /*------------------------------------------------------------------------------
@@ -167,7 +241,6 @@ const AD_AdiEntryType ABCC_API_asAdiEntryList[] =
 **------------------------------------------------------------------------------
 */
 const AD_MapType ABCC_API_asAdObjDefaultMap[] =
-//AD_MapType sDefaultMap[] =
 {
     /*
     ** -------------------------------------------------------------
@@ -185,105 +258,14 @@ const AD_MapType ABCC_API_asAdObjDefaultMap[] =
 
 
 /*------------------------------------------------------------------------------
-** Example - just for avoiding compiler warnings    
+** Example - just for avoiding compiler warnings
 **------------------------------------------------------------------------------
 */
 void ABCC_API_CbfCyclicalProcessing()
 {
 }
-
-UINT16 ABCC_API_CbfGetNumAdi(void)
-{
-    return(sizeof(ABCC_API_asAdiEntryList) / sizeof(AD_AdiEntryType));
-}
 /******************************************************************************************************/
-
-
-/******************************************************************************************************/
-#else       //ABCC_CFG_DRV_STRUCTURED_ADIS
-//added to support structured ADIs; end
-
-
-
-
-
-#if (  ABCC_CFG_STRUCT_DATA_TYPE_ENABLED || ABCC_CFG_ADI_GET_SET_CALLBACK_ENABLED )
-   #error ABCC_CFG_ADI_GET_SET_CALLBACK_ENABLED must be set to 0 and ABCC_CFG_STRUCT_DATA_TYPE_ENABLED set to 0 in order to run this example
 #endif
-
-/*------------------------------------------------------------------------------
-** Data holder for the network data parameters (ADI)
-**------------------------------------------------------------------------------
-*/
-uint16_t appl_iSpeed;
-uint16_t appl_iRefSpeed;
-
-/*------------------------------------------------------------------------------
-** Min, max and default value for appl_aiUint16
-**------------------------------------------------------------------------------
-*/
-static AD_UINT16Type appl_sUint16Prop = { { 0, 0xFFFF, 0 } };
-
-/*-------------------------------------------------------------------------------------------------------------
-** 1. iInstance | 2. pabName | 3. bDataType | 4. bNumOfElements | 5. bDesc | 6. pxValuePtr | 7. pxValuePropPtr
-**--------------------------------------------------------------------------------------------------------------
-*/
-const AD_AdiEntryType ABCC_API_asAdiEntryList[] =
-{
-   {  0x1,  "SPEED",     ABP_UINT16,   1, AD_ADI_DESC___W_G, { { &appl_iSpeed,    &appl_sUint16Prop } } },
-   {  0x2,  "REF_SPEED", ABP_UINT16,   1, AD_ADI_DESC__R_S_, { { &appl_iRefSpeed, &appl_sUint16Prop } } }
-};
-
-
-/*------------------------------------------------------------------------------
-** Example - electric motor control loop
-**------------------------------------------------------------------------------
-*/
-void ABCC_API_CbfCyclicalProcessing()
-{
-   if( ABCC_API_AnbState() == ABP_ANB_STATE_PROCESS_ACTIVE )
-   {
-      /*
-      ** An example of ADI data handling.
-      */
-      if( appl_iSpeed > appl_iRefSpeed )
-      {
-         /*
-         ** Do something that lowers speed.
-         */
-         appl_iSpeed -= 1;
-      }
-      else if( appl_iSpeed < appl_iRefSpeed )
-      {
-         /*
-         ** Do something that increases speed.
-         */
-         appl_iSpeed += 1;
-      }
-   }
-   else
-   {
-      /*
-      ** We are not in process active, the default should be that the motor
-      ** should not run.
-      */
-      appl_iSpeed = 0;
-   }
-}
-/*------------------------------------------------------------------------------
-** Map all adi:s in both directions
-**------------------------------------------------------------------------------
-** 1. AD instance | 2. Direction | 3. Num elements | 4. Start index |
-**------------------------------------------------------------------------------
-*/
-const AD_MapType ABCC_API_asAdObjDefaultMap[] =
-{
-   { 1, PD_WRITE, AD_MAP_ALL_ELEM, 0 },
-   { 2, PD_READ,  AD_MAP_ALL_ELEM, 0 },
-   { AD_MAP_END_ENTRY }
-};
-
-/******************************************************************************************************/
 
 
 
@@ -291,10 +273,4 @@ UINT16 ABCC_API_CbfGetNumAdi(void)
 {
     return(sizeof(ABCC_API_asAdiEntryList) / sizeof(AD_AdiEntryType));
 }
-
-
-//added to support structured ADIs; start
-#endif
-//added to support structured ADIs; end
-
 
